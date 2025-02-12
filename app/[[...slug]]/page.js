b4717d473b290dd1f5a2e0293895d991ae79f7f0
Kevin <kevinvar2805@gmail.com>
2025-02-12 15:52:02 -0600

    several fixes

--- app/[[...slug]]/page.js
+++ app/[[...slug]]/page.js
@@ -3,8 +3,8 @@ import React from 'react';
 import axios from 'axios';
 /*global process*/
 const Page = async () => {
-  const product = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/products/${40344}`,{headers: {Authorization:process.env.NEXT_PUBLIC_AUTH_SECRET} }).then((response) => response.data);
-  const overview = await axios.get(process.env.NEXT_PUBLIC_API_URL + `/products/${40344}/styles`,{headers: {Authorization:process.env.NEXT_PUBLIC_AUTH_SECRET} }).then((result)=>{
+  const product = await axios.get(`${process.env.API_URL}/products/${40344}`,{headers: {Authorization:process.env.AUTH_SECRET} }).then((response) => response.data);
+  const overview = await axios.get(process.env.API_URL + `/products/${40344}/styles`,{headers: {Authorization:process.env.AUTH_SECRET} }).then((result)=>{
     if(result.data.results){
       return {gallery: result.data.results[0], picture: result.data.results[0].photos[0].url, styles: result.data.results, price: '$' + product.default_price}
     }
